66acef21b68dbabe5cfde447b974bc0b8cbe00bd
Supraja <59463956+SuprajaKamalapuram@users.noreply.github.com>
2022-11-08 11:10:13 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err, "it has an error"));
+  });
+  return (
+    <section className="App">
+      <h1>Image uploading react</h1>
+      {data.map((singleData) => {
+        // const base64String = btoa(
+        //   String.fromCharCode(...new Uint8Array(singleData.img.data.data))
+        // );
+        
+        const base64String = Array.from(new Uint8Array(singleData.img.data.data), byte => String.fromCharCode(byte)).join("");
+        const theImage = btoa(base64String);
+        return <img className="upload" src={`data:image/png;base64,${theImage}`}/>
+      })}
+    </section>
+  );
+}
+
+export default App;
